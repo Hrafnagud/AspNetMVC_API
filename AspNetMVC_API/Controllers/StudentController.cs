50e994936b6b92d7df83a4a62ab601a3930966f0
Emre Ç <superbevent@hotmail.com>
2022-02-07 13:05:10 +0300

    Add project files.

---
+++ AspNetMVC_API/Controllers/StudentController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using AspNetMVC_API.Models.ViewModels;
+using AspNetMVC_API_BusinessLogicLayer.Repository;
+using AspNetMVC_API_Entity.Models;
+
+namespace AspNetMVC_API.Controllers
+{
+    public class StudentController : ApiController
+    {
+        StudentRepo studentRepo = new StudentRepo();
+
+        public ResponseData GetAll()
+        {
+            try
+            {
+                var list = studentRepo.GetAll().Select(x => new { x.Id, x.Name, x.Surname, x.RegisterDate }).ToList();
+
+                if (list != null)
+                {
+                    if (list.Count == 0)
+                    {
+                        return new ResponseData()
+                        {
+                            Success = true,
+                            Message = "There are no registered student yet!",
+                            Data = list
+                        };
+                    }
+                    return new ResponseData()
+                    {
+                        Success = true,
+                        Message = "All Student list has been successfully sent!",
+                        Data = list
+                    };
+                }
+                else
+                {
+                    return new ResponseData()
+                    {
+                        Success = false,
+                        Message = "Error has occured while fetching all stundents list!"
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ResponseData()
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+        }
+
+        public ResponseData GetById(int id)
+        {
+            try
+            {
+                if (id > 0)
+                {
+                    var student = studentRepo.GetById(id);
+                    if (student == null)
+                    {
+                        throw new Exception($"There are no records for id {id}!");
+                    }
+
+                    return new ResponseData()
+                    {
+                        Success = true,
+                        Message = "Record has been found!",
+                        Data = new { student.Id, student.Name, student.Surname, student.RegisterDate }
+                    };
+                }
+                else
+                {
+                    return new ResponseData()
+                    {
+                        Success = false,
+                        Message = "Negative value can not be sent!"
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ResponseData()
+                {
+                    Success = false,
+                    Message = ex.Message
+                };
+            }
+        }
+    }
+}
---
+++ AspNetMVC_API_Entity/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetMVC_API_Entity.Models   //Class Library (.net framwork)
+{
+    [Table("Students")]
+    public class Student
+    {
+        [Key]
+        [Column(Order = 1)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must consist characters between 2-50. (Both Inclusive)")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Surname must consist characters between 2-50. (Both Inclusive)")]
+        public string Surname { get; set; }
+
+        [Required]
+        public DateTime RegisterDate { get; set; } = DateTime.Now;
+    }
+}

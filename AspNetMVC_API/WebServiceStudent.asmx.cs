cf52200441dcd4c103184250f30fe212b20e85d4
Emre Ç <superbevent@hotmail.com>
2022-02-08 11:05:09 +0300

    WebService object is being tested for insert and getall using soap

--- AspNetMVC_API/WebServiceStudent.asmx.cs
+++ AspNetMVC_API/WebServiceStudent.asmx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AspNetMVC_API_BusinessLogicLayer.Repository;
+using AspNetMVC_API_Entity.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,11 +18,58 @@ namespace AspNetMVC_API
     // [System.Web.Script.Services.ScriptService]
     public class WebServiceStudent : System.Web.Services.WebService
     {
+        StudentRepo studentRepo = new StudentRepo();
 
         [WebMethod]
         public string HelloWorld()
         {
             return "Hello World";
+        }
+
+        [WebMethod]
+        public List<Student> GetAll()
+        {
+            try
+            {
+                List<Student> list = studentRepo.GetAll();
+                return list;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        [WebMethod]
+        public string Insert(string name, string surname)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(surname))
+                {
+                    throw new Exception("Both name and surname must be provided!");
+                }
+
+                Student newStudent = new Student()
+                {
+                    Name = name,
+                    Surname = surname
+                };
+                int insertResult = studentRepo.Insert(newStudent);
+                if (insertResult > 0)
+                {
+                    //First Approach
+                    return "Record has been successfully saved. Id = " + newStudent.Id;
+                }
+                else
+                {
+                    throw new Exception("An unexpected error has occured during record insertion!");
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
     }
 }

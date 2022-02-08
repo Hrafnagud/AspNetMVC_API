ce786289e5fe1e5b85208599adaba4f4982201a8
Emre Ç <superbevent@hotmail.com>
2022-02-08 11:51:23 +0300

    CRUD operators are implemented and tested for WebServiceStudent.asmx.cs

--- AspNetMVC_API/WebServiceStudent.asmx.cs
+++ AspNetMVC_API/WebServiceStudent.asmx.cs
@@ -1,5 +1,6 @@
 ﻿using AspNetMVC_API_BusinessLogicLayer.Repository;
 using AspNetMVC_API_Entity.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -59,11 +60,95 @@ namespace AspNetMVC_API
                 if (insertResult > 0)
                 {
                     //First Approach
-                    return "Record has been successfully saved. Id = " + newStudent.Id;
+                    //return "Record has been successfully saved. Id = " + newStudent.Id;
+
+                    //Second Approach
+                    string jsonString = JsonConvert.SerializeObject(newStudent);
+                    return jsonString;
                 }
                 else
                 {
                     throw new Exception("An unexpected error has occured during record insertion!");
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        [WebMethod]
+        public string Delete(int id)
+        {
+            try
+            {
+                if (id > 0)
+                {
+                    Student student = studentRepo.GetById(id);
+                    if (student == null)
+                    {
+                        throw new Exception("There is no such studentd. Delete operation failed!");
+                    }
+                    int deleteResult = studentRepo.Delete(student);
+                    if (deleteResult > 0)
+                    {
+                        return "Record has been successfully deleted.";
+                    }
+                    else
+                    {
+                        throw new Exception("An unexpected error has occured during record deletion process!");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Provided id must be greater than zero");
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        [WebMethod]
+        public string Update(int currentId, string newName, string newSurname)
+        {
+            try
+            {
+                if (currentId <= 0)
+                {
+                    throw new Exception("Provided id must be greater than zero!");
+                }
+                if (string.IsNullOrEmpty(newName) && string.IsNullOrEmpty(newSurname))
+                {
+                    throw new Exception("Both name and surname fields for update, must be updated!");
+                }
+                Student currentStudent = studentRepo.GetById(currentId);
+                if (currentStudent == null)
+                {
+                    throw new Exception("No student found. Update operation failed!");
+                }
+
+                //If newName parameter is not null, update will take place.
+                if (!string.IsNullOrEmpty(newName))
+                {
+                    currentStudent.Name = newName;
+                }
+
+                //If newSurname parameter is not null, update will take place.
+                if (!string.IsNullOrEmpty(newSurname))
+                {
+                    currentStudent.Surname = newSurname;
+                }
+
+                int updateResult = studentRepo.Update();
+                if (updateResult > 0)
+                {
+                    return "Record has been successfully updated.";
+                }
+                else
+                {
+                    throw new Exception("An unexpected error has occured. Record update failed.");
                 }
             }
             catch (Exception ex)
